696300f63d458f3974b81b5af66fb779e6b7b495
Ray Luna <leon.luna.ray@gmail.com>
2024-12-27 18:08:12 -0800

    staticfiles settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath } from 'url'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,8 +9,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../backend/src/static'),
   },
 })
