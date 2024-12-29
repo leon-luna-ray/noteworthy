5a6a7c907ca5052db1c8489473715432b85ba8bd
Ray Luna <leon.luna.ray@gmail.com>
2024-12-28 22:01:44 -0800

    check if user prev registered

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -22,18 +22,21 @@ oauth2_bearer = OAuth2PasswordBearer(tokenUrl="authToken")
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    existing_user =  db.query(Users).filter(Users.email == create_user_request.email).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
     create_user_model = Users(
         email=create_user_request.email,
         password=bcrypt_context.hash(create_user_request.password),
     )
+    
     db.add(create_user_model)
     db.commit()
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
-):
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
