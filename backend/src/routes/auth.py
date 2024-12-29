4410666eda7cc628c69cf7a108d4a964fb3ca4c3
Ray Luna <leon.luna.ray@gmail.com>
2024-12-28 22:46:43 -0800

    whoami route

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -17,26 +17,31 @@ SECRET_KEY = config("SECRET_KEY")
 ALGORITHM = config("ALGORITHM")
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="authToken")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="api/v1/auth/token")
 
-
+# /api/v1/auth/register
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
-    existing_user =  db.query(Users).filter(Users.email == create_user_request.email).first()
+    existing_user = (
+        db.query(Users).filter(Users.email == create_user_request.email).first()
+    )
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+        )
 
     create_user_model = Users(
         email=create_user_request.email,
         password=bcrypt_context.hash(create_user_request.password),
     )
-    
     db.add(create_user_model)
     db.commit()
 
-
+# /api/v1/auth/token
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
@@ -86,7 +91,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 
     return {"email": email, "user_id": user_id}
 
-
+# /api/v1/auth/whoami
 @router.get("/whoami", status_code=status.HTTP_200_OK)
 async def whoami(current_user: Annotated[dict, Depends(get_current_user)]):
     if not current_user:
