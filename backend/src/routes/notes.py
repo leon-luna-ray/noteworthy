221542bbd6c397264186cdb263a7ab15c8d91cba
Ray Luna <leon.luna.ray@gmail.com>
2024-12-29 22:42:53 -0800

    note post to db sucessful

--- backend/src/routes/notes.py
+++ backend/src/routes/notes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+
 from typing import List, Annotated
+from datetime import datetime
+
 from .. import models, schemas
 from ..database import get_db, db_dependency
 from .auth import get_current_user
@@ -15,10 +18,10 @@ def get_notes(db: db_dependency):
 
 # /api/v1/notes/new
 @router.post("/new/", response_model=schemas.Note)
-def create_note(note: schemas.NoteCreate, db: db_dependency):
-    print("🍒 DO SOMETHING")
-    db_note = models.Notes(**note.dict())
+def create_note(note: schemas.NoteCreate, db: db_dependency, current_user: Annotated[dict, Depends(get_current_user)]):
+    db_note = models.Notes(**note.dict(), user_id=current_user['user_id'], created_at=datetime.now(), updated_at=datetime.now())
     db.add(db_note)
     db.commit()
     db.refresh(db_note)
+    
     return db_note
--- backend/src/schemas/notes.py
+++ backend/src/schemas/notes.py
@@ -4,13 +4,16 @@ from datetime import datetime
 class NoteBase(BaseModel):
     title: str
     body: str
-    user_id: int
+
 
 class NoteCreate(NoteBase):
     pass
 
 class Note(NoteBase):
     id: int
-
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+    
     class Config:
         from_attributes = True
