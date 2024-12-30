028a54b32cc23ac004ccf0ac722b4d66d45a231b
Ray Luna <leon.luna.ray@gmail.com>
2024-12-29 23:42:05 -0800

    update home page

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div className="container flex flex-col gap-y-[4rem] justify-center items-center">
+    <div className="container flex flex-col gap-y-[6rem] justify-center items-center">
       <div className="flex-col-1 justify-center items-center">
         <h1>Noteworthy</h1>
         <p className='h4'>A Note-taking web app.</p>
